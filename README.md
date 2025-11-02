261a40e9a8ffb8bc18dbf131a11eb8401e2f6c43
kaiping <kaiping7895@gmail.com>
2025-11-02 12:34:21 +0800

    update readme for fullbuild seperate the logic for config and build so fullbuild can reuse the logic

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ This is a project template for cpp project with cmake build system
 2) Run poetry install in project root directory to install all the needed python package
 3) poetry run python -m buildtool.build configure
 4) poetry run python -m buildtool.build build
+
+
+# Notes
+- poetry run python -m buildtool.build fullbuild to build release and debug version together
--- buildtool/build.py
+++ buildtool/build.py
@@ -18,6 +18,36 @@ def run_command(cmd):
     echo(f"Running: {' '.join(cmd)}")
     subprocess.check_call(cmd)
 
+def configLogic(buildType):
+    is_msvc = "VisualStudioVersion" in os.environ or "VSINSTALLDIR" in os.environ
+    os.makedirs(__BUILD_DIR, exist_ok=True)
+    if(is_msvc):
+        run_command(["cmake", "-S", ".", "-B", __BUILD_DIR, f"-DBUILD_TYPE_CACHE={buildType}"])
+    else:
+        run_command(["cmake", "-S", ".", "-B", __BUILD_DIR+"/"+buildType, f"-DBUILD_TYPE_CACHE={buildType}"])
+
+def buildLogic(target, build_type = None):
+    if build_type is not None:
+        cmd = ["cmake", "--build", __BUILD_DIR+"/"+build_type]
+    else:
+        cmd = ["cmake", "--build", __BUILD_DIR]
+    is_msvc = "VisualStudioVersion" in os.environ or "VSINSTALLDIR" in os.environ
+    if is_msvc:
+        # Read from CMake cache
+        cache_file = os.path.join(__BUILD_DIR, "CMakeCache.txt")
+        if os.path.exists(cache_file):
+            with open(cache_file, "r") as f:
+                for line in f:
+                    if line.startswith("BUILD_TYPE_CACHE:"):
+                        build_type = line.split('=')[1].strip()
+                        break
+        if build_type:
+            cmd += ["--config", build_type]
+
+    if target:
+        cmd += ["--target", target]
+    run_command(cmd)
+
 
 
 @click.group()
@@ -38,31 +68,27 @@ def clean():
 @cli.command()
 @click.option("--build-type", "buildType",default="Debug", type=click.Choice(["Debug", "Release"]))        
 def configure(buildType):
-    os.makedirs(__BUILD_DIR, exist_ok=True)
-    run_command(["cmake", "-S", ".", "-B", __BUILD_DIR, f"-DBUILD_TYPE_CACHE={buildType}"])
+    configLogic(buildType)
 
 @cli.command()
 @click.option("--target", default=None)
-def build(target):
-    cmd = ["cmake", "--build", __BUILD_DIR]
+def build(target, build_type = None):
+    buildLogic(target,build_type)
+
+@cli.command()
+@click.option("--target", default=None)
+def fullbuild(target):
     is_msvc = "VisualStudioVersion" in os.environ or "VSINSTALLDIR" in os.environ
-    build_type = None
     if is_msvc:
-        # Read from CMake cache
-        cache_file = os.path.join(__BUILD_DIR, "CMakeCache.txt")
-        if os.path.exists(cache_file):
-            with open(cache_file, "r") as f:
-                for line in f:
-                    if line.startswith("BUILD_TYPE_CACHE:"):
-                        build_type = line.split('=')[1].strip()
-                        break
-        if build_type:
-            cmd += ["--config", build_type]
-
-    if target:
-        cmd += ["--target", target]
-    run_command(cmd)
-
+        for config in ["Debug", "Release"]:
+            cmd = ["cmake", "--build", __BUILD_DIR, "--config", config]
+            if target:
+                cmd += ["--target", target]
+            run_command(cmd)
+    else:
+        for build_type in ["Debug", "Release"]:
+            configLogic(build_type)
+            buildLogic(target, build_type)
 
 
 if __name__ == "__main__":
