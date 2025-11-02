3f676e87b43fecd1db0aeb4ffa3820ad079a4bca
kaiping <kaiping7895@gmail.com>
2025-11-02 10:17:40 +0800

    make this template to be able to build across linux and windows

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # cppProjectTemplate
 This is a project template for cpp project with cmake build system
+
+# Setup
+1) Install poetry
+2) Run poetry install in project root directory to install all the needed python package
+3) poetry run python -m buildtool.build configure
+4) poetry run python -m buildtool.build build
--- src/mylib.cpp
+++ src/mylib.cpp
@@ -1,6 +1,12 @@
 #include "mylib.h"
 #include <iostream>
 
+#ifdef _WIN32
 void mylib_function() {
-    std::cout << "Hello from MyLib!" << std::endl;
+  std::cout << "Hello from MyLib in windows!" << std::endl;
 }
+#elif defined(__linux__)
+void mylib_function() {
+  std::cout << "Hello from MyLib in linux!" << std::endl;
+}
+#endif
